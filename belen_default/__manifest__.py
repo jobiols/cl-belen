7319d1422eab69a5265fc9a031f05bdbe269a0c6
jobiols <jorge.obiols@gmail.com>
2019-12-09 13:31:26 -0300

    [ADD] commit inicial

---
+++ belen_default/__manifest__.py
@@ -0,0 +1,64 @@
+# -----------------------------------------------------------------------------
+#
+#    Copyright (C) 2019  jeo Software  (http://www.jeosoft.com.ar)
+#    All Rights Reserved.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# -----------------------------------------------------------------------------
+{
+    'name': 'Belen',
+    'version': '12.0e.0.0.0',
+    'license': 'Other OSI approved licence',
+    'category': 'Default Application',
+    'summary': 'Customization for Grupo Belen',
+    'author': 'jeo Software',
+    'depends': [
+        # basic applications
+        'sale_management',
+        'account_invoicing',
+        'purchase',
+
+        # minimum modules for argentinian localizacion + utilities + fixes
+        # 'standard_depends',
+
+    ],
+    'data': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'images': [],
+
+    #
+    # Here begins docker-odoo-environment manifest
+    # --------------------------------------------
+
+    # port where odoo starts serving pages
+    'port': '8069',
+    # example repos version 1
+    'repos': [
+        {'usr': 'jobiols', 'repo': 'cl-belen', 'branch': '12.0'},
+    ],
+
+    # example images version 1
+    'docker': [
+        {'name': 'odoo', 'usr': 'jobiols', 'img': 'odoo-ent', 'ver': '12.0e'},
+        {'name': 'postgres', 'usr': 'postgres', 'ver': '11.1-alpine'},
+        {'name': 'nginx', 'usr': 'nginx', 'ver': 'latest'},
+    ],
+
+}
